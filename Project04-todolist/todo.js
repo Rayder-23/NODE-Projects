b2e7953d58b6721363df36b4544a9b3a152f3727
Rayder <165102003+Rayder-23@users.noreply.github.com>
2024-04-01 04:47:01 +0500

    added project04

---
+++ Project04-todolist/todo.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = []; //array
+let condition = true;
+while (condition == true) {
+    let addTask = await inquirer.prompt([
+        {
+            message: "What would you like to add to your ToDo List?",
+            type: 'input',
+            name: 'todo'
+        },
+        {
+            message: "Would you like to add more?",
+            type: 'confirm',
+            name: 'addMore',
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
